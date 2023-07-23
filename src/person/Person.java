166116be73af0e7a0a3f0637ad2be6aee3efb1e7
ilyaTATARENTSEV <kaxaaapbg@mail.ru>
2023-07-24 01:37:59 +0300

    HomeWork JAVA

---
+++ src/person/Person.java
@@ -0,0 +1,16 @@
+class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    // Переопределение метода toString()
+    @Override
+    public String toString() {
+        return name;
+    }
+}
